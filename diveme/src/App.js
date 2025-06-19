49fcbb87d8fc6f421dd8e61d0b8925fd59092b6d
Pasindu Randula <pasindusadeep046@gmail.com>
2025-06-19 23:47:36 +0530

    routing app js home page , nav bar and footer

--- diveme/src/App.js
+++ diveme/src/App.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import LoginPage from './components/login page/login'; 
 import RegisterPage from './components/register page/register';
+import HomePage from './components/home page/home';
+import Navbar from './components/nav bar/navbar';
+import Footer from './components/footer/footer';
+
+function AppContent() {
+  const location = useLocation();
+  const hideNavbarRoutes = ['/', '/register'];
+
+  const shouldHideNavbar = hideNavbarRoutes.includes(location.pathname);
+
+  return (
+    <div className="app-container">
+      {!shouldHideNavbar && <Navbar />}
+      
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
+
+      {!shouldHideNavbar && <Footer />}
+    </div>
+  );
+}
 
 function App() {
   return (
     <Router>
-      <div className="app-container">
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-        </Routes>
-      </div>
+      <AppContent />
     </Router>
   );
 }
