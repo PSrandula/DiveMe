f2c9879c928a95077ba96030a4423d1237c9f845
Pasindu Randula <pasindusadeep046@gmail.com>
2025-06-28 17:46:28 +0530

    create cloudanary api

---
+++ diveme-backend/utils/cloudinary.js
@@ -0,0 +1,10 @@
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: "dzxvvyijj",
+  api_key: "328843722616233",
+  api_secret: "3HSlEk7d9k2f_aAvp2T0Mnuu1h0",
+  secure: true,
+});
+
+module.exports = cloudinary;
